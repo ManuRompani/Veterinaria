4a0a076b0e55c09897cf5b0bcbdd8beccf19667b
Emiliano Acosta <emilianoam167@gmail.com>
2025-11-24 20:21:29 -0300

    Se agrega modelos Cliente y Usuario/ se agrega la carpeta .vs al gitIgnore

---
+++ Veterinaria/Services.Veterinaria/model/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Veterinaria.Model
+{
+    public class Usuario
+    {
+        public string UserName {  get; set; }
+        public string UserPassword {  get; set; }
+
+    }
+}
