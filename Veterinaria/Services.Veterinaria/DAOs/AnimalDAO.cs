1ff623c34eba73a4442b57026b2194b11c894801
Yamila Holik <shami.nair.holik@gmail.com>
2025-11-27 21:30:57 -0300

    Creacion AnimalDAO

---
+++ Veterinaria/Services.Veterinaria/DAOs/AnimalDAO.cs
@@ -0,0 +1,123 @@
+﻿using Services.Veterinaria.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Veterinaria.DAOs
+{
+    public class AnimalDAO : GenericDAO
+    {
+        /// <summary>
+        /// Método para recorrer la DB y devolver una lista con todos los animales
+        /// </summary>
+        /// <returns></returns>
+        public List<Animal> getAllAnimals() {
+
+            var ListaAnimales = new List<Animal>();
+
+            string consulta = "SELECT ID, Nombre, Peso, Edad, Cliente_ID, Especie_ID FROM Animales";
+
+            setearConsulta(consulta);
+            ejecutarLectura();
+
+            while(_lector.Read()) {
+                Animal animal = new Animal()
+                {
+                    ID = _lector.GetInt32(0),
+                    Nombre = _lector.GetString(1),
+                    Peso = _lector.GetDecimal(2),
+                    Edad = _lector.GetInt32(3),
+                    Cliente = new Cliente { Dni = _lector.GetInt32(4) },
+                    Especie = new Especie { ID = _lector.GetInt32(5) }
+                };
+                ListaAnimales.Add(animal);
+            }
+
+            desconectar();
+
+            return ListaAnimales;
+        }
+
+
+
+        /// <summary>
+        /// Metodo que recibe dos parametros para buscar por rango de edad en la base de datos.
+        /// Devuelve el maximo, minimo y promedio de peso agrupados por especie.
+        /// </summary>
+        /// <param name="edad1"></param>
+        /// <param name="edad2"></param>
+        /// <returns></returns>
+        public DataTable getPorRangoEdad(int edad1, int edad2) {
+
+            string consulta = $@"SELECT
+                            Especies.Nombre AS Especie,
+                            MAX(Animales.Peso) AS PesoMaximo,
+                            MIN(Animales.Peso) AS PesoMinimo,
+                            AVG(Animales.Peso) AS PesoPromedio
+                            FROM 
+                            Animales a
+                            JOIN 
+                            Especies e ON a.Especie_ID = e.ID
+                            WHERE 
+                            a.Edad BETWEEN @edadMin AND @edadMax
+                            GROUP BY 
+                            e.Nombre;";
+
+           
+
+            setearConsulta(consulta);
+            
+            //Esto vendria a ser el rango que ingresa el cliente en el textBox
+            insertarParametro("@edadMin", edad1.ToString());
+            insertarParametro("@edadMax", edad2.ToString());
+
+            ejecutarLectura();
+
+            SqlDataAdapter da = new SqlDataAdapter((SqlCommand)_comando);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            desconectar();
+
+            return dt;
+        
+        }
+
+
+
+        /// <summary>
+        /// Metodo para insertar un animal en la tabla Animales de la base de datos
+        /// </summary>
+        /// <param name="animal"></param>
+        /// <returns></returns>
+        public bool insertAnimal(Animal animal) {
+            string edad = animal.Edad.ToString();
+            string query = $@"INSERT INTO Animales (Nombre, Peso, Edad, Cliente_ID, Especie_ID)
+VALUES (@nombre, @peso, @edad, @clienteId, @especieId);";
+
+            setearConsulta(query);
+
+            insertarParametro("@nombre", animal.Nombre);
+            insertarParametro("@peso", animal.Peso.ToString());
+            insertarParametro("@nombre", animal.Edad.ToString());
+            insertarParametro("@nombre", animal.Cliente.Dni.ToString());
+            insertarParametro("@nombre", animal.Especie.ID.ToString());
+
+            ejecutarLectura();
+
+            int RowsAffected = _comando.ExecuteNonQuery();
+
+            desconectar();
+
+
+            // Si RowsAffected es mayor a 0, entonces se inserto correctamente
+            return RowsAffected > 0;
+        }
+
+
+    }
+}
